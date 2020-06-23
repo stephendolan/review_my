ee156c514ec423319184e0eaec7085926f1c1817
Stephen Dolan <dolan.stephen1@gmail.com>
2020-06-23 17:01:36 -0400

    Convert to UUIDS

--- src/models/base_model.cr
+++ src/models/base_model.cr
@@ -1,4 +1,9 @@
 abstract class BaseModel < Avram::Model
+  macro default_columns
+    primary_key id : UUID
+    timestamps
+  end
+
   def self.database : Avram::Database.class
     AppDatabase
   end
