b87497d1d719e8f3fd7ae2ff34f2046573678042
Stephen Dolan <dolan.stephen1@gmail.com>
2020-06-27 15:52:18 -0400

    Make show page more useful

--- src/pages/me/show_page.cr
+++ src/pages/me/show_page.cr
@@ -1,5 +1,11 @@
 class Me::ShowPage < MainLayout
   def content
-    h1 "This is your profile"
+    h1 "Hey, #{current_user.email}!", class: "text-lg text-center"
+
+    para class: "text-center mt-10" do
+      text "You can't do too much right now... but you can "
+
+      link "request a password reset", to: PasswordResetRequests::New, class: "text-teal-500 hover:text-gray-800"
+    end
   end
 end
