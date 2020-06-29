8f42b3b8f67c4ddb717501894b5254d20a96dd81
Stephen Dolan <dolan.stephen1@gmail.com>
2020-06-29 19:17:29 -0400

    Refine some rough edges

--- src/pages/snippets/index_page.cr
+++ src/pages/snippets/index_page.cr
@@ -4,7 +4,7 @@ class Snippets::IndexPage < MainLayout
   def content
     h1 "Your Snippets", class: "text-center mt-4 mb-10 text-xl font-bold"
 
-    div class: "grid md:grid-cols-2 lg:grid-cols-3 gap-4" do
+    div class: "grid md:grid-cols-2 lg:grid-cols-3 gap-5" do
       add_snippet_card
 
       snippets.each do |snippet|
@@ -14,7 +14,7 @@ class Snippets::IndexPage < MainLayout
   end
 
   private def snippet_card(snippet)
-    div class: "h-32 border rounded bg-gray-200 flex flex-col justify-between" do
+    div class: "h-32 border border-gray-400 rounded bg-gray-200 flex flex-col justify-between" do
       div class: "relative mt-2" do
         div snippet.title, class: "text-center font-semibold"
         link to: Snippets::Show.with(snippet.slug) do
@@ -29,9 +29,9 @@ class Snippets::IndexPage < MainLayout
   private def copy_link_section(snippet)
     url = Snippets::Show.with(snippet.slug).url
 
-    div class: "text-center bg-gray-400 flex justify-between items-center", data_controller: "clipboard" do
+    div class: "text-center bg-indigo-100 border-t-2 flex justify-between items-center", data_controller: "clipboard" do
       input class: "py-1 bg-transparent focus:outline-none w-full text-xs pl-4", value: url, readonly: true, data_target: "clipboard.source"
-      button class: "text-xs h-full px-4 border-l bg-indigo-500 border-indigo-600 hover:bg-indigo-600 text-white font-semibold flex items-center space-x-2", data_action: "click->clipboard#copy" do
+      button class: "text-xs h-full px-4 border-l border-indigo-600 #{hoverable_indigo} font-semibold flex items-center space-x-2", data_action: "click->clipboard#copy" do
         i class: "far fa-copy"
         span "Copy"
       end
@@ -40,11 +40,14 @@ class Snippets::IndexPage < MainLayout
 
   private def add_snippet_card
     flex = "flex items-center justify-center"
-    colors = "bg-indigo-500 hover:bg-indigo-600 text-white"
 
-    link to: Snippets::New, class: "h-32 border rounded #{colors} #{flex} space-x-2" do
+    link to: Snippets::New, class: "h-32 border rounded #{hoverable_indigo} #{flex} space-x-2" do
       i class: "fas fa-plus-circle"
       span "Create a new snippet", class: "font-extrabold"
     end
+  end
+
+  private def hoverable_indigo
+    "bg-indigo-500 hover:bg-indigo-600 text-white"
   end
 end
