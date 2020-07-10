3f93d78eb7f2a9de8522fcd39ce4689c29f26f12
Stephen Dolan <dolan.stephen1@gmail.com>
2020-07-10 11:40:59 -0400

    Refactor marketing page away from CSS grid to flex

--- src/pages/marketing/index_page.cr
+++ src/pages/marketing/index_page.cr
@@ -1,38 +1,23 @@
 class Marketing::IndexPage < AuthLayout
   def content
-    div class: "px-3 lg:px-0 select-none" do
-      div class: "relative bg-white overflow-hidden" do
-        div class: "relative pt-6 pb-16 md:pb-20 lg:pb-24 xl:pb-32" do
-          div class: "mt-8 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-12 xl:mt-24" do
-            div class: "lg:grid lg:grid-cols-12 lg:gap-8" do
-              div class: "sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left" do
-                h2 class: "mt-1 text-4xl tracking-tight leading-10 font-bold text-gray-900 sm:leading-none sm:text-6xl lg:text-5xl" do
-                  text "Need a quick review?"
-                end
-                para class: "mt-3 text-base text-gray-600 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl" do
-                  text "ReviewMy helps you submit quick snippets for review from others, ensuring that you get the feedback you need quickly, concisely, and intuitively. Use it for emails, text messages, portions of larger documents, or anything else based on text!"
-                end
-                div class: "mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start" do
-                  div class: "rounded-md shadow" do
-                    link "Get Started", to: SignUps::New, class: "w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-gray-100 bg-indigo-600 hover:bg-indigo-500 hover:text-gray-100 focus:outline-none focus:shadow-outline md:py-4 md:text-lg md:px-10"
-                  end
-                  div class: "ml-3 inline-flex" do
-                    link "Sign In", to: SignIns::New, class: "w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-gray-100 bg-indigo-600 hover:bg-indigo-500 hover:text-gray-100 focus:outline-none focus:shadow-outline md:py-4 md:text-lg md:px-10"
-                  end
-                end
-              end
-              div class: "mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center" do
-                div class: "relative mx-auto w-full lg:max-w-md" do
-                  img src: "https://media.giphy.com/media/KDDb6Kh2zUhhaD4x0P/giphy.gif", class: "shadow-lg rounded-lg"
-                end
-              end
-            end
+    div class: "select-none" do
+      div class: "px-6 my-20 lg:my-32 flex flex-col lg:flex-row text-center lg:text-left items-center lg:space-x-12 space-y-12 lg:space-y-0" do
+        div class: "md:max-w-2xl" do
+          render_main_title
+
+          render_title_subtext
+
+          div class: "flex justify-center lg:justify-start space-x-3 mt-6" do
+            render_action_button(title: "Get Started", link: SignUps::New)
+            render_action_button(title: "Sign In", link: SignIns::New)
           end
         end
+
+        render_app_preview_gif
       end
 
       div class: "bg-gray-200 py-8" do
-        div class: "my-8 mx-auto max-w-screen-xl px-4 sm:my-12 sm:px-6 md:my-20 xl:my-24" do
+        div class: "my-8 mx-auto max-w-screen-xl px-4 sm:my-12 sm:px-6 lg:my-20 xl:my-24" do
           div class: "text-center" do
             h2 "How does it work?", class: "font-bold text-4xl"
           end
@@ -53,6 +38,28 @@ class Marketing::IndexPage < AuthLayout
           end
         end
       end
+    end
+  end
+
+  private def render_app_preview_gif
+    img src: "https://media.giphy.com/media/KDDb6Kh2zUhhaD4x0P/giphy.gif", class: "shadow-lg rounded-lg"
+  end
+
+  private def render_action_button(title, link)
+    div do
+      link title, to: link, class: "px-8 py-3 border border-transparent rounded-md text-gray-100 bg-indigo-600 hover:bg-indigo-500 hover:text-gray-100 focus:outline-none focus:shadow-outline lg:py-4 lg:text-lg lg:px-10"
+    end
+  end
+
+  private def render_main_title
+    h2 class: "text-4xl tracking-tight leading-10 font-bold text-gray-900 sm:leading-none sm:text-5xl" do
+      text "Need a quick review?"
+    end
+  end
+
+  private def render_title_subtext
+    para class: "mt-3 text-base text-gray-600 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl" do
+      text "ReviewMy helps you submit quick snippets for review from others, ensuring that you get the feedback you need quickly, concisely, and intuitively. Use it for emails, text messages, portions of larger documents, or anything else based on text!"
     end
   end
 
