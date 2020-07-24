6d6b30eefeda48d651952282ef2ae189d3cbfaa1
Stephen Dolan <dolan.stephen1@gmail.com>
2020-07-24 09:30:48 -0400

    Remove diff controller for now

--- src/pages/snippets/revisions/new_page.cr
+++ src/pages/snippets/revisions/new_page.cr
@@ -7,7 +7,7 @@ class Snippets::Revisions::NewPage < PublicLayout
   end
 
   def content
-    div data_controller: "diff" do
+    div data_controller: "" do
       m SnippetComponent, snippet: snippet
 
       div class: "hidden", data_target: "diff.old" do
