dc2303c7ed3713e5efe5bd4027ab8df169a72028
Stephen Dolan <dolan.stephen1@gmail.com>
2020-07-26 17:31:08 -0400

    Add link to StephenCodes instead of plain text

--- src/components/shared/footer.cr
+++ src/components/shared/footer.cr
@@ -2,7 +2,8 @@ class Shared::Footer < BaseComponent
   def render
     nav class: "w-full flex items-center justify-between bg-gray-100 border-t p-2 select-none" do
       span class: "text-xs font-semibold" do
-        text "© #{Time.local.year} StephenCodes"
+        text "© #{Time.local.year} "
+        a "StephenCodes", href: "https://stephencodes.com", target: "_blank", class: link_color
       end
 
       span class: "text-xs font-semibold" do
