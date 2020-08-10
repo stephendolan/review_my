f8835fa0d5b8e015a55cafbbba9b4da1d91afd46
Stephen Dolan <dolan.stephen1@gmail.com>
2020-08-09 20:44:20 -0400

    Fix margins

--- src/components/revision/diff_view.cr
+++ src/components/revision/diff_view.cr
@@ -6,7 +6,7 @@ class Revision::DiffViewer < BaseComponent
     div class: "border-t border-gray-200 bg-gray-50 px-4 py-4 sm:px-6", data_controller: "diff" do
       link to: Snippets::Revisions::Show.with(snippet.slug, revision), class: "text-sm hover:text-gray-600" do
         span "Revision by"
-        span revision.author, class: "font-medium"
+        span revision.author, class: "ml-1 font-medium"
       end
 
       div class: "hidden", data_target: "diff.old" do
